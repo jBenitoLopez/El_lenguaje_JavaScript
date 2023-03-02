6f2b2f3f0193102525935ca4ec433960f058e769
jBenitoLopez <lopez.jbenito@gmail.com>
2023-03-02 20:39:44 +0100

    fix: diaSemanaEropeo fn

--- 5-11_fecha_y_hora/diaSemanaEropeo.js
+++ 5-11_fecha_y_hora/diaSemanaEropeo.js
@@ -12,19 +12,33 @@ día de la semana “europeo” para la variable date.
 let date = new Date(2012, 0, 3);  // 3 Jan 2012
 alert( getLocalDay(date) );       // tuesday, should show 2
 */
-const LOCAL_DAYS = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Thursday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let date = new Date(2012, 0, 3); // 3 Jan 2012
+// const LOCAL_DAYS = [
+//   "Sunday",
+//   "Monday",
+//   "Tuesday",
+//   "Thursday",
+//   "Thursday",
+//   "Friday",
+//   "Saturday",
+// ];
+// let date = new Date(2012, 0, 3); // 3 Jan 2012
+
+// function getLocalDay(date) {
+//   return LOCAL_DAYS[date.getDay()];
+// }
+
+// console.log(getLocalDay(date)); // tuesday, should show 2
 
 function getLocalDay(date) {
-  return LOCAL_DAYS[date.getDay()];
+  let day = date.getDay();
+
+  if (day == 0) {
+    // weekday 0 (sunday) is 7 in european
+    day = 7;
+  }
+
+  return day;
 }
 
+let date = new Date(2012, 0, 3); // 3 Jan 2012
 console.log(getLocalDay(date)); // tuesday, should show 2
